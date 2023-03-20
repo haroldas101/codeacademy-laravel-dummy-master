8a0ebdac5048012b7704115f2ca0485890f4eaae
haroldas101 <haroldasvaic@gmail.com>
2023-03-20 21:02:52 +0200

    new

---
+++ src/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+// Route::get('/{id}', function ($id): string {
+//     return 'hello world';
+// });
+
+Route::get('/{name}', function ($name): string {
+    return "hello $name";
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::name('products.')->prefix('/products')->controller(ProductController::class)->group(function () {
+    Route::get('/','index')->name('index');
+});
+
+Route::name('orders.')->prefix('/orders')->controller(OrderController::class)->group(function () {
+    Route::get('/','index')->name('index');
+});
+
+Route::get('/contact',function () {
+    return view('contact');
+})->name('contact');
